bcde8d061fdcd36eba7a09afe008002a8019f1b7
Mr.Wang <evilperfect87@gmail.com>
2016-03-05 17:46:47 +0800

    set colour for echo text by echo -e cmd

--- src/ShellCode/gitPullForAllRepositories.sh
+++ src/ShellCode/gitPullForAllRepositories.sh
@@ -5,24 +5,22 @@
 #                and put this script under the directory and execute it. 
 
 dir="/cygdrive/d/open-src"
-#ls $dir
+ls $dir
 arr=`ls ${dir}`
 
-echo -e "\033[31m  ready for git pull \033[0m"
-#echo "------------------cut here---------------"
+echo "ready for git pull"
+echo "------------------cut here---------------"
 for i in $arr
 do
-#echo "find file="$i
+echo "find file="$i
   if [ -d $i ]
   then
-    #cd $i
-    #echo "L1 pwd ="$PWD
-	echo -e "\033[31m "
+    cd $i
+    echo "L1 pwd ="$PWD
     git pull origin master
     cd ..
-    #echo "L2 pwd ="$PWD
+    echo "L2 pwd ="$PWD
   fi
 done
-
-#echo "------------------cut here---------------"
+echo "------------------cut here---------------"
 
